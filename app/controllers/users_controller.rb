d2c6ff91dd02949e6d77a97d73159c5504b5676b
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-09-22 22:33:24 +1000

    added route for current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user
+
+  def current
+    render json: {name: current_user.first_name}
+    # unless current_user == nil
+    #   render json: current_user.first_name
+    # else
+    #   render json: nil
+    # end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #Knock user token creator
-  post 'user_token' => 'user_token#create'
+  post '/user_token' => 'user_token#create'
 
+  #User routes
+  get '/users/current' => 'users#current'
 
 end
